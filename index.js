643d4b8335b4877db79cb1ca7c494bf9992a30f6
Mortexe <paris.chrysanthou@gmail.com>
2018-01-18 22:04:31 +0000

    username/color objects

--- index.js
+++ index.js
@@ -11,10 +11,51 @@ app.get('/', (req,res) => {
   res.sendFile(__dirname + '/public/index.html');
 });
 
+var userCount = 0;
+var uniqueID = -1;
+var users = {
+  priv : [],
+  public : []
+};
+
 io.on('connection', (socket) => {
   console.log('user connected');
+
+  uniqueID++;
+  var priv = {};
+
   socket.on('message', (msg) =>{
-    console.log(`message: ${msg}`);
-    io.emit('message', msg);
-  })
-})
+    var user = users.public[priv.uniqueID];
+
+    io.emit('message', {
+      "msg": msg,
+      "color": user.color,
+      "time": new Date().getHours() + ":" + new Date().getMinutes() + ":" + new Date().getSeconds(),
+      "nick": user.nick
+  });
+});
+
+
+	socket.on('command', function(req)
+	{
+		console.log(req);
+	});
+
+socket.on('updateProfile', function(data)
+	{
+		users.public[priv.uniqueID] = data;
+		io.sockets.emit('sysmessage', { "users" : users.public, "count" : usercount});
+	});
+
+  socket.on('onConnect', function(data)
+	{
+
+
+		priv.uniqueID = uniqueID;
+		users.public[priv.uniqueID] = data;
+
+		// io.emit('uniqueID', user.uniqueID);
+		io.sockets.emit('sysmessage', { "users" : users.public});
+	});
+
+});
